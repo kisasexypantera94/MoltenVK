b69670bb5d24c3b8b02509e75ddd1b7b24041347
Aitor Camacho <aitor@lunarg.com>
2024-08-23 04:33:11 +0900

    Add VK_EXT_external_memory_metal

--- MoltenVK/MoltenVK/GPUObjects/MVKDevice.h
+++ MoltenVK/MoltenVK/GPUObjects/MVKDevice.h
@@ -331,7 +331,7 @@ public:
 	VkExternalMemoryProperties& getExternalBufferProperties(VkExternalMemoryHandleTypeFlagBits handleType);
 
 	/** Returns the external memory properties supported for images for the handle type. */
-	VkExternalMemoryProperties& getExternalImageProperties(VkExternalMemoryHandleTypeFlagBits handleType);
+	VkExternalMemoryProperties& getExternalImageProperties(VkFormat format, VkExternalMemoryHandleTypeFlagBits handleType);
 
 	
 #pragma mark Metal
@@ -819,6 +819,8 @@ public:
 
 	/** Returns the Metal objects underpinning the Vulkan objects indicated in the pNext chain of pMetalObjectsInfo. */
 	void getMetalObjects(VkExportMetalObjectsInfoEXT* pMetalObjectsInfo);
+
+	MTLResource_id getResourceIdFromHandle(const VkMemoryGetMetalHandleInfoEXT* pGetMetalHandleInfo, MTLResource_id* pHandle) const;
 
 
 #pragma mark Properties directly accessible
