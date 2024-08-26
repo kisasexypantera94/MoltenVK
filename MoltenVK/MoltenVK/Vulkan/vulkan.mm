450d074fa9019ada3aaa0262135aac80bcc2a38e
Aitor Camacho <aitor@lunarg.com>
2024-08-26 10:33:50 +0900

    Implement vkGetMemoryMetalHandlePropertiesEXT

--- MoltenVK/MoltenVK/Vulkan/vulkan.mm
+++ MoltenVK/MoltenVK/Vulkan/vulkan.mm
@@ -2783,7 +2783,8 @@ MVK_PUBLIC_VULKAN_SYMBOL VkResult vkGetMemoryMetalHandlePropertiesEXT(
 																	  VkMemoryMetalHandlePropertiesEXT*     pMemoryMetalHandleProperties) {
 
 	MVKTraceVulkanCallStart();
-	// TODO
+	MVKDevice* mvkDvc = MVKDevice::getMVKDevice(device);
+	pMemoryMetalHandleProperties->memoryTypeBits = mvkDvc->getPhysicalDevice()->getExternalResourceMemoryTypeBits(handle);
 	MVKTraceVulkanCallEnd();
 	return VK_SUCCESS;
 }
